9e2ab4cc5b15985ce9cf8dd70758ae34abf292ac
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-02-15 17:31:13 -0600

    problems5-7

--- closures.js
+++ closures.js
@@ -126,10 +126,12 @@ counter.dec() // 12
 
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
-    return function
+     function message() {
+      return welcomeText + ' ' + firstname + ' ' + lastname + '.';
+    }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -156,10 +158,17 @@ var module = (function() {
 
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
-  return {
-    // Code here.
+  return { 
+    publicMethod: function(){
+      return privateMethod();
+    }
   };
 })();
+  module.publicMethod();
+
+
+
+
 
 
 
@@ -176,7 +185,15 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: function(parameter){
+      secret += parameter;
+      return secret;
+    },
+
+    takeAwayFromSecret: function(parameter) {
+      secret -= parameter;
+      return secret;
+    }
   };
 }
 
